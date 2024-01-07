638aad5a9633477fbde0bd4a1456892f75b4c1ab
Vishal-Varkhede <Vishal-Varkhede>
2024-01-07 06:27:37 +0000

    Final Gold Tables are created

--- Databricks-To-PowerBI-Pipeline/Creation_Of_Gold_Tables_From_Silver_Tables.py
+++ Databricks-To-PowerBI-Pipeline/Creation_Of_Gold_Tables_From_Silver_Tables.py
@@ -10,18 +10,14 @@ product_data_silver  = spark.sql("select * from default.product_data_silver")
 
 # COMMAND ----------
 
-
-
-# COMMAND ----------
-
 try:
-    # sales_data_silver.write.format("delta").option("mergeSchema", "true").mode('overwrite').saveAsTable("default.sales_data_gold")  
+    sales_data_silver.write.format("delta").option("mergeSchema", "true").mode('overwrite').saveAsTable("default.sales_data_gold")  
 
-    # order_data_silver.write.format("delta").option("mergeSchema", "true").mode('overwrite').saveAsTable("default.order_data_gold")  
+    order_data_silver.write.format("delta").option("mergeSchema", "true").mode('overwrite').saveAsTable("default.order_data_gold")  
 
-    # customer_data_silver.write.format("delta").option("mergeSchema", "true").mode('overwrite').saveAsTable("default.customer_data_gold")
+    customer_data_silver.write.format("delta").option("mergeSchema", "true").mode('overwrite').saveAsTable("default.customer_data_gold")
 
-    # product_data_silver.write.format("delta").option("mergeSchema", "true").mode('overwrite').saveAsTable("default.product_data_gold")
+    product_data_silver.write.format("delta").option("mergeSchema", "true").mode('overwrite').saveAsTable("default.product_data_gold")
     status = "Success"
 except Exception as e:
   
